06ad6d2e404f0ada9d5e7b953938838e8da9bb90
ganesh9803 <santhosh.ganesh1023@gmail.com>
2025-01-04 17:39:27 +0530

    up

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,11 +1,11 @@
-import Navbar from "./components/Navbar"
-import Sidebar from "./components/sidebar"
+import Navbar from "./components/Navbar.jsx"
+import Sidebar from "./components/Sidebar.jsx"
 import { Routes, Route } from "react-router-dom"
-import Add from './pages/Add'
-import List from './pages/List'
-import Orders from './pages/Orders'
+import Add from './pages/Add.jsx'
+import List from './pages/List.jsx'
+import Orders from './pages/Orders.jsx'
 import { useEffect, useState } from "react"
-import Login from "./components/Login"
+import Login from "./components/Login.jsx"
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
